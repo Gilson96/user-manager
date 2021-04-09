1fa4b537b44566b422a399f48c4cc12c9ae03308
Gilson96 <grafael99@gmail.com>
2021-04-09 01:22:39 +0100

    MySQL database created

--- database/migrations/2021_04_08_211214_create_clients_table.php
+++ database/migrations/2021_04_08_211214_create_clients_table.php
@@ -15,6 +15,10 @@ class CreateClientsTable extends Migration
     {
         Schema::create('clients', function (Blueprint $table) {
             $table->id();
+            $table->string("first_name", 50);
+            $table->string("last_name", 50);
+            $table->string("username", 20);
+            $table->boolean("dark_mode");
             $table->timestamps();
         });
     }
